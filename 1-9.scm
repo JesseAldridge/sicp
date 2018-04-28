6e80b0e7b107377bef15f1e336739574dabf212e
Jesse Aldridge <jesse.aldridge@airbnb.com>
2018-04-28 15:24:30 -0700

    1-9

---
+++ 1-9.scm
@@ -0,0 +1,26 @@
+(define (inc x) (+ x 1))
+(define (dec x) (- x 1))
+
+(define (plus a b)
+  (if (= a 0)
+      b
+      (inc (plus (dec a) b))))
+
+; (plus 2 3)
+; (inc (plus (dec 2) 3))
+; (inc (plus 1 3))
+; (inc (inc (plus (dec 1) 3)))
+; (inc (inc (plus 0 3)))
+; (inc (inc 3))
+; (inc 4)
+; 5
+
+(define (plus a b)
+  (if (= a 0)
+      b
+      (plus (dec a) (inc b))))
+
+; (plus 2 3)
+; (plus 1 4)
+; (plus 0 5)
+; 5
